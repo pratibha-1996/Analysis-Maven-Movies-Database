2747cdd13e2122d507342124f865bb992807a4ee
Pratibha Gairola <130970726+pratibha-1996@users.noreply.github.com>
2023-10-22 01:43:31 +0530

    Update sql.

--- sql.sql
+++ sql.sql
@@ -1,24 +1,48 @@
--- Write a SQL query to count the number of characters except the spaces for each actor. Return first 10 actors name length along with their name.
+-- Write an SQL query to count the number of characters except the spaces for each actor. Return first 10 actors name length along with their name.
 Select length(concat(first_name,last_name)) as length, concat(first_name,' ',last_name) as Name
 from mavenmovies.actor
 order by length desc
 limit 10;
 
--- List all Oscar awardees(Actors who received Oscar award) with their full names and length of their names.
+-- List all Oscar awardees(Actors who received Oscar awards) with their full names and length of their names.
+Select length(concat(first_name,last_name)) as length, concat(first_name,' ',last_name) as Name
+from actor_award
+where awards like "%Oscar%";
 
 -- Find the actors who have acted in the film ‘Frost Head’.
+SELECT FIRST_NAME,LAST_NAME FROM ACTOR A
+INNER JOIN FILM_ACTOR FA
+ON A.ACTOR_ID=FA.ACTOR_ID
+INNER JOIN FILM F
+ON FA.FILM_ID=F.FILM_ID
+WHERE F.TITLE="FROST HEAD";
 
 -- Pull all the films acted by the actor ‘Will Wilson’.
+SELECT F.TITLE FROM ACTOR A
+INNER JOIN FILM_ACTOR FA
+ON A.ACTOR_ID=FA.ACTOR_ID
+INNER JOIN FILM F
+ON FA.FILM_ID=F.FILM_ID
+WHERE A.FIRST_NAME="WILL" AND A.LAST_NAME="WILSON";
 
 -- Pull all the films which were rented and return in the month of May.
+SELECT TITLE FROM FILM F
+INNER JOIN INVENTORY T 
+ON F.FILM_ID=T.FILM_ID
+INNER JOIN RENTAL R
+ON T.INVENTORY_ID=R.INVENTORY_ID
+WHERE MONTH(R.RENTAL_DATE) =5 AND MONTH(R.RETURN_DATE) =5;
 
 -- Pull all the films with ‘Comedy’ category.
-
-
+SELECT F.TITLE FROM FILM F
+INNER JOIN FILM_CATEGORY FC
+ON F.FILM_ID=FC.FILM_ID
+INNER JOIN CATEGORY C
+ON FC.CATEGORY_ID=C.CATEGORY_ID
+WHERE C.NAME="COMEDY";
 
 -- Write a SQL query to find the actors who played a role in the movie 'Annie IDENTITY’. Return all the fields of the actor table.
 
-USE mavenmovies;
 SELECT A.FIRST_NAME,A.LAST_NAME FROM ACTOR A
 INNER JOIN FILM_ACTOR FA
 ON A.ACTOR_ID=FA.ACTOR_ID
